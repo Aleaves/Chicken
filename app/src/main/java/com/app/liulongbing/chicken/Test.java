8dfcad3363cf91f1cb202cfe81018190a3cd9724
Aleaves <2849459897@qq.com>
2017-01-19 17:53:34 +0800

    master4

--- app/src/main/java/com/app/liulongbing/chicken/Test.java
+++ app/src/main/java/com/app/liulongbing/chicken/Test.java
@@ -1,11 +1,23 @@
 package com.app.liulongbing.chicken;
 
+import android.util.Log;
+
 /**
  * Created by liulongbing on 17/1/18.
  */
 
 public class Test {
-    //66677
 
+    //master
+    //123mm
+    public void getName(){
+        Log.i("=======","master00000");
+        //dev
+    }
+
+
+
+
+    //////
 
 }
